e5dfc54dcfe2f0be4b8fb5d1abc41a4b0cd05b8a
Nikola2412 <nikolamark50@gmail.com>
2022-12-02 19:00:22 +0100

    bugs

--- js.js
+++ js.js
@@ -1,6 +1,18 @@
 let desni = document.querySelector('.desni');
-for(let i = 0;i<50;i++){
+for(let i = 0;i<10;i++){
+    let div = document.createElement('div');
     let img = document.createElement('img');
     img.setAttribute("src","./icons/history.png");
     desni.appendChild(img);
 }
+
+document.querySelector('.lines').addEventListener('click',el=>{
+    let bar = document.querySelector('.levi');
+    //console.log(bar.className.split(' ')[1]);
+    if(bar.className.split(' ')[1] == 'produzen'){
+        bar.className = 'levi skracen'
+    }
+    else{
+        bar.className = 'levi produzen'
+    }
+});
