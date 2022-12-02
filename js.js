2ec87a8700b1bcfee954a0ec7d85e0ab4ebf5c5a
Nikola2412 <nikolamark50@gmail.com>
2022-12-02 12:12:26 +0100

    sections

---
+++ js.js
@@ -0,0 +1,6 @@
+let desni = document.querySelector('.desni');
+for(let i = 0;i<50;i++){
+    let img = document.createElement('img');
+    img.setAttribute("src","./icons/history.png");
+    desni.appendChild(img);
+}
