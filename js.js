97eb3165181930229467b46083dbe289f28d60ac
Nikola2412 <nikolamark50@gmail.com>
2022-12-03 21:48:39 +0100

    logika

--- js.js
+++ js.js
@@ -1,19 +1,46 @@
-window.alert('Jos nije gotov sajt')
+//window.alert('Jos nije gotov sajt')
 let desni = document.querySelector('.desni');
-for(let i = 0;i<10;i++){
+for(let i = 0;i<0;i++){
     let div = document.createElement('div');
     let img = document.createElement('img');
     img.setAttribute("src","./icons/history.png");
     desni.appendChild(img);
 }
 
+
+
+const min = 734;
+let levi = document.querySelector('.levi');
+
 document.querySelector('.lines').addEventListener('click',el=>{
-    let bar = document.querySelector('.levi');
-    //console.log(bar.className.split(' ')[1]);
-    if(bar.className.split(' ')[1] == 'produzen'){
-        bar.className = 'levi skracen'
+
+    if(window.screen.width < min){
+        return;
+    }
+    //console.log(levi.className.split(' ')[1]);
+    if(levi.className.split(' ')[1] == 'produzen'){
+        promeni();
     }
     else{
-        bar.className = 'levi produzen'
+        levi.className = 'levi produzen'
     }
 });
+
+function promeni(){
+    levi.className = 'levi skracen';
+}
+
+function resize() {
+    //if(window.screen.width >= min)
+    //    return;
+    //promeni();
+    //console.log(levi.getBoundingClientRect().bottom);
+    //console.log(levi.offsetHeight);
+    //console.log(window.screen.height);
+    //levi.addEventListener('scroll',el=>{
+    //    console.log('da');
+    //});
+}
+if(window.screen.width < min)
+    promeni();
+window.onresize = resize;
