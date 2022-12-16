23d7bf6803fd97393ba2247c6a64325e9ff7da9a
Nikola2412 <83644688+Nikola2412@users.noreply.github.com>
2022-12-16 11:34:47 +0100

    Update videi.js

--- srcipts/videi.js
+++ srcipts/videi.js
@@ -1,8 +1,10 @@
 let clips = document.querySelectorAll('video').forEach(clip=>{
     clip.addEventListener("mouseover", function (e) {
         clip.play();
+        clip.loop();
     })
     clip.addEventListener("mouseout", function (e) {
         clip.pause();
+        clip.currentTime=0;
     })
 });
