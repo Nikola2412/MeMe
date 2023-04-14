720be6e9149211ba1451fc051653a108ca08009b
Nikola2412 <83644688+Nikola2412@users.noreply.github.com>
2023-04-14 15:12:37 +0200

    nmp

--- public/srcipts/upload.js
+++ public/srcipts/upload.js
@@ -11,7 +11,7 @@ dropzone.addEventListener("drop", (event) => {
     event.preventDefault();
     var dFiles = event.dataTransfer.files;
 
-    
+    let k = files.length;
     for (let i = 0; i < dFiles.length; i++) {
       const file = dFiles[i];
       const reader = new FileReader();
@@ -31,23 +31,32 @@ dropzone.addEventListener("drop", (event) => {
           close.src = './ico/close.png';
           btn.appendChild(close);
           img.className = 'imgpre';
-          img.id = 'slika';
+          img.id = 'slika ' + (k + i);
           img.src = event.target.result;
           img.setAttribute('type','image');
           img.setAttribute('name','meme');
           div.appendChild(btn);
           div.appendChild(img);
           btn.addEventListener('click',el=>{
+            let removeEl = btn.nextElementSibling.id.split(' ')[1];
+            const indexToDelete = files.findIndex(obj => obj.id == removeEl);
+            files.splice(indexToDelete, 1);
             btn.parentElement.remove();
           });
           preview.appendChild(div);
-          files.push(dFiles[i]);
+          files.push(
+            {
+              "id"    :(k + i),
+              "Slika" :dFiles[i]
+            }
+            );
         }else {
           alert('Samo slike u jpg ili png formatu')
         }
       });
       reader.readAsDataURL(file);
-    }  
+    }
+    console.log(files);
   });
 const imgInp = document.getElementById('imgInp');
 dropzone.addEventListener(
@@ -62,7 +71,7 @@ dropzone.addEventListener(
 
   imgInp.onchange = evt => {
     const [file] = imgInp.files
-    console.log(file);
+    //console.log(file);
     if (file) {
       const div = document.createElement('div');
       div.id = 'spoj';
@@ -74,17 +83,25 @@ dropzone.addEventListener(
       close.src = './ico/close.png';
       btn.appendChild(close);
       img.className = 'imgpre';
-      img.id = 'slika';
+      img.id = 'slika ' + (files.length);
       img.src = URL.createObjectURL(file);
       img.setAttribute('type','image');
       img.setAttribute('name','meme');
       div.appendChild(btn);
       div.appendChild(img);
       btn.addEventListener('click',el=>{
+        let removeEl = btn.nextElementSibling.id.split(' ')[1];
+        const indexToDelete = files.findIndex(obj => obj.id == removeEl);
+        files.splice(indexToDelete, 1);
         btn.parentElement.remove();
       });
       preview.appendChild(div);
-      files.push(file);
+      files.push(
+        {
+          "id"    :(files.length),
+          "Slika" :file
+        }
+      );
     }
   }
 
@@ -104,10 +121,15 @@ function imageToByteArray(file) {
 }
   
 
-async function  Upload(){
-  console.log(files.length);
+function  Upload(){
+  let n = 0;
+  console.log(files);
     for (let i = 0; i < files.length; i++) {
-        const img = files[i];
+        const img = files[i].Slika;
+        const id = files[i].id;
+        //console.log(document.getElementById('slika ' + id).parentElement);
+        //console.log(id);
+        
         imageToByteArray(img).then(byteArray => {
           var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
           var theUrl = "/upload";
@@ -119,8 +141,12 @@ async function  Upload(){
           //https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/status
         }).catch(error => {
           alert(error);
+        }).finally(el=>{
+            const indexToDelete = files.findIndex(obj => obj.id == id);
+            files.splice(indexToDelete, 1);
+            document.getElementById('slika ' + id).parentElement.remove();
         });
+        
   }
-  this.location.href = '/';
+  //this.location.href = '/';
 }
-
