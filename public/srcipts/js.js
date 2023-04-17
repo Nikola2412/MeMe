769c8a7e92fd08efbce19ad50b8a901ff9119e08
Nikola2412 <83644688+Nikola2412@users.noreply.github.com>
2023-04-17 13:27:14 +0200

    bagovi

--- public/srcipts/js.js
+++ public/srcipts/js.js
@@ -3,22 +3,14 @@ let levi = document.querySelector('.levi');
 document.querySelector('.lines').addEventListener('click',el=>{
 
     if(window.screen.width < 450){
-        //console.log(levi.style.display);
         promeni();
-        //alert(levi.style.marginLeft);
-        //console.log(levi.style.marginLeft);
         if(levi.style.marginLeft =='0px')
             levi.style.marginLeft ='-100px';
         else
             levi.style.marginLeft  = '0'
-        //if (levi.style.display === 'none') {
-        //    levi.style.display = 'block';
-        //}else {
-        //    levi.style.display = 'none';
-        //}
+
     }
     else{
-        //console.log(levi.className.split(' ')[1]);
         levi.style.marginLeft  = '0'
         if(levi.className.split(' ')[1] == 'produzen'){
             promeni();
@@ -35,15 +27,7 @@ function promeni(){
 }
 
 function resize() {
-    //if(window.screen.width >= min)
-    //    return;
-    //promeni();
-    //console.log(levi.getBoundingClientRect().bottom);
-    //console.log(levi.offsetHeight);
-    //console.log(window.screen.height);
-    //levi.addEventListener('scroll',el=>{
-    //    console.log('da');
-    //});
+
 }
 if(window.screen.width < 450)
     promeni();
@@ -58,7 +42,7 @@ input.addEventListener("keypress", el=> {
 
 let close = document.querySelector('.close');
 function out(el){
-    if(window.screen.width > 815)
+    if(window.screen.width > 814)
         return;
     document.querySelectorAll('.nav div').forEach(div=>{
         div.style.display = 'flex';
@@ -79,41 +63,45 @@ close.addEventListener('click',el=>{
     out(document.querySelector('.search'));
 });
 
-window.addEventListener("orientationchange", event => {
-    if (window.screen.width >= 450) {
-        levi.style.display = 'block';
-        levi.style.marginLeft ='0px'
-    }    
-});
 document.querySelector('.nalog').addEventListener('click',el=>{
     let options = document.querySelector('.desni .options');
-    //options.style.animation="test 1s linear";
     if(options.style.marginRight=='20px')
         options.style.marginRight = '-150px';
     else{
         options.style.marginRight = '20px';
     }
-    //if(options.style.display == 'none'){
-    //    options.style.display = 'block';
-    ////    options.className.split(' ')[1]='opac1'
-    //    options.style.animation="test 1s linear";
-    //}
-    //else{
-    //    options.style.animation ="reverse test 1s linear";
-    ////    options.className.split(' ')[1]='opac0'
-    //    setTimeout(() => {
-    //        options.style.display = 'none';
-    //    }, "1000");
-    //    
-    //}
+
 });
-/*
-document.querySelectorAll('.desni .logo').forEach(slika =>{
-    slika.addEventListener('click',el=>{
-        this.location.href = `/chanal?id=${slika.id}`;
-    });
-});
-*/
+
 window.onorientationchange = (event) => {
     promeni();
-  };
+    if (window.screen.width >= 450) {
+        levi.style.display = 'block';
+        levi.style.marginLeft ='0px'
+    }
+};
+
+window.onload = (event) => {
+  let os = navigator.userAgent;
+  if (os.search('Windows')!==-1){
+  }
+  else if (os.search('Mac')!==-1){
+  }
+  else if (os.search('Linux')!==-1 && os.search('X11')!==-1){
+  }
+  else if (os.search('Android')!==-1){
+  }
+}
+/*
+window.onresize = (event) =>{
+    if(window.screen.width <= 815)
+        return;
+    document.querySelectorAll('.nav div').forEach(div=>{
+        div.style.display = 'flex';
+    });
+    document.querySelector('.search').style.display =  'block';
+    document.querySelector('.close').style.display =  'none';
+    document.querySelector('.lupa').style.display =  'none';
+    
+}
+*/
